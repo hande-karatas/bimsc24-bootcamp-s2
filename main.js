a80a3494f780e1c866892596d4e19beda384d0c2
hande-arch <156059093+hande-karatas@users.noreply.github.com>
2024-01-17 17:10:33 +0300

    revisions

---
+++ main.js
@@ -0,0 +1,8 @@
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "Hello world!";
+let myVariable;
+myVariable = "Hande"
+
+document.querySelector("html").addEventListener("click", function () {
+    alert("Ouch! Stop poking me!");
+  });
